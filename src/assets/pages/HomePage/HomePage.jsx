86bd37fe01f443fdc9b2a0b7f1e5a32928b4ec71
Han Hsun Shih (Amy) <a23514788@gmail.com>
2024-12-30 19:35:00 +0000

    Add styles file, build home page

--- src/assets/pages/HomePage/HomePage.jsx
+++ src/assets/pages/HomePage/HomePage.jsx
@@ -1,10 +1,29 @@
+import "./HomePage.scss";
+import heroImage from "../../images/hero-image.png";
+import { Link } from "react-router-dom";
+
 export default function HomePage() {
   return (
     <>
-      <h1>
-        Hi! <br />
-        I'm Amy, Han Hsun Shih
-      </h1>
+      <div className="hero">
+        <img className="hero__image" src={heroImage} alt="hero image" />
+        <h1 className="hero__text">
+          Hi! <br />
+          I'm Amy, Han Hsun Shih
+        </h1>
+        <Link to="/about" className="hero__button-box">
+          <p className="hero__button">More About Me</p>
+        </Link>
+      </div>
+      <article className="article">
+        <h2 className="article__title">Full-Stack Project</h2>
+        <p>
+          This QR code ordering app is built for my sister's cafe in Taiwan,
+          allowing customers to order online. The client side handles menu
+          display and ordering, while the admin dashboard, with login access,
+          lets authorized users view orders and history.
+        </p>
+      </article>
     </>
   );
 }
