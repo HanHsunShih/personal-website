9973e13d64e05d11140230c9a2ad2bbaed421116
Han Hsun Shih (Amy) <a23514788@gmail.com>
2025-01-09 20:05:53 +0800

    Add content in data analysis page

--- src/pages/DataAnalysis/DataAnalysis.jsx
+++ src/pages/DataAnalysis/DataAnalysis.jsx
@@ -1,5 +1,7 @@
 import "./DataAnalysis.scss";
 import calendarImg from "../../assets/images/calendar.png";
+import postcards from "../../assets/images/2postcards.png";
+import redEnvelop from "../../assets/images/red-envelop.jpg";
 
 export default function DataAnalysis() {
   return (
@@ -18,7 +20,7 @@ export default function DataAnalysis() {
       <main className="data-page__main">
         <article className="data-page__article">
           <img
-            className="data-page__calendar-img"
+            className="data-page__img"
             src={calendarImg}
             alt="calendar image"
           />
@@ -54,6 +56,42 @@ export default function DataAnalysis() {
             <h3>sales revenue</h3>
           </section>
         </article>
+        <article className="data-page__article">
+          <h2 className="data-page__title">
+            Problem Statement 1:
+            <br /> Increase Visitors
+          </h2>
+          <p className="data-page__content">
+            Despite follower growth, our post reach and shop visitors haven’t
+            kept pace. How can we create content that better engages followers
+            and drives them to our online shop?
+          </p>
+        </article>
+        <article className="data-page__article">
+          <h2 className="data-page__title">Hypothesis</h2>
+          <p className="data-page__content">
+            By sharing more content about the story of the product and the
+            product development process, likes on posts and visits to our online
+            shop might increase.
+          </p>
+          <p>Promotional Photo vs. Design process image</p>
+          <img className="data-page__img" src={postcards} alt="2 postcards" />
+          <p className="data-page__Hypothesis-conclusion">
+            The basis of this hypothesis is that our Instagram and Facebook
+            insights show that content about the design process and product
+            inspiration engages our followers the most.
+          </p>
+        </article>
+        <article className="data-page__article">
+          <h2 className="data-page__title">A/B test</h2>
+          <p className="data-page__content">
+            Goal: Increase post reach and guide visitors to the online shop from
+            social media
+          </p>
+          <h2 className="data-page__title--light-blue">Control Group 50%</h2>
+          <img className="data-page__img" src={redEnvelop} alt="Red Envelop" />
+        </article>
+        <article className="data-page__article"></article>
         <article className="data-page__article"></article>
         <article className="data-page__article"></article>
       </main>
